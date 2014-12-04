88c0b88c952bfaa840283f16375f049aba7964fc
Peter Souter <p.souter@kainos.com>
2014-12-04 16:37:35 +0000

    Re-enable test to see if cache_busting fixes

--- spec/classes/bar_idempotent_spec.rb
+++ spec/classes/bar_idempotent_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe "example::bar" do
-  before { skip 'Known Issue, currently not working' }
-
   describe "first in-line hiera_data test with a" do
     let(:hiera_data) { { :bar_message => "a" } }
     it { should contain_notify("bar").with_message("a") }
