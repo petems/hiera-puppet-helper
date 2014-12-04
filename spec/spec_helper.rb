efa2be99566b9a2bddd782a503744201b0e4978c
Peter Souter <p.souter@kainos.com>
2014-12-04 16:37:17 +0000

    Add :cache_bust based on @dcarley's suggestion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,7 @@ require File.join(File.dirname(__FILE__), '../lib/hiera-puppet-helper')
 RSpec.configure do |c|
   c.order = "random"
 end
+
+shared_context "default values" do
+  let(:hiera_data) { { :cache_bust => Time.now } }
+end
